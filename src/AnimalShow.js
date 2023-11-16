31085094e0ba8b220cd5bab5012c6b674c6af5c2
Abu Suraj <abusurajbd@gmail.com>
2023-11-17 04:45:35 +0600

    implement props property, object to minimize if-else conidtion

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,5 +1,27 @@
-function AnimalShow() {
-  return <div>Cow!</div>;
+import './AnimalShow.css'
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import horse from './svg/horse.svg';
+import heart from './svg/heart.svg';
+import { useState } from 'react';
+
+const animalImage = {
+  bird, cat, dog, cow, gator,horse
+}
+function AnimalShow({type}) {
+  const [click, setClick] = useState(0)
+  const handleClick = () =>{
+    setClick(click+1)
+  }
+  return (
+  <div className='animal-show' onClick={handleClick}>
+    <h1>{type}</h1>
+    <img className='animal' src={animalImage[type]} alt={type}/>
+    <img className='heart' style={{width:10+10*click}} src={heart} alt='heart'/>
+  </div>);
 }
 
 export default AnimalShow;
--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+import './App.css'
+function getRandomAnimal(){
+  const animals = ['bird', 'cat', 'cow','dog', 'gator', 'horse'];
+  return animals[Math.floor(Math.random() * animals.length) ]
+}
+// console.log(getRandomAnimal());
+
 function App() {
+  let id = 0;
+ const [animals, setAnimals] = useState([])
+console.log(animals);
   const handleClick = () =>{
-    console.log('cliked');
+    setAnimals([...animals, getRandomAnimal()]); 
   }
-  return (<div>
-    <button onClick={handleClick()}>Add animal</button>
+  return (
+  <div className="app">
+    <button onClick={ handleClick}>Add animal</button>
+    {animals.map(animal =>
+     <AnimalShow key={id++} type={animal}></AnimalShow>
+     // <span key={id++}>{animal} </span>
+
+    )
+    }
   </div>);
 }
 
